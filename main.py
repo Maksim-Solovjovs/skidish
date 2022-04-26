7e9b35b844bb6e2193ee4a97e8748b8d316cd650
Max <77140683+Maksim-Solovjovs@users.noreply.github.com>
2022-04-26 09:43:17 +0000

    s

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ a = int (input("Ievadi skaitļu: "))
 b = int (input("Ievad vel vieno skaitli: "))
 if a >= b:
   print (a+b)
-  print ("Uzdevums paveikts!")
+  print ("bumbabam")
 elif b>a:
   print (a*b)
-  print ("Uzdevums paveikts!")
+  print ("skidish")
